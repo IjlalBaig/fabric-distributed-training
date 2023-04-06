621ad743a11dbb0f678e5419b2cf3486544ce902
zaryabmakram <zaryabmakram@gmail.com>
2023-04-06 22:56:48 +0500

    initial commit

---
+++ model.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+
+class EncoderDecoder(nn.Module):
+    def __init__(self):
+        super(EncoderDecoder, self).__init__()
+        
+        self.encoder = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        
+        self.decoder = nn.Sequential(
+            nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.Upsample(scale_factor=2),
+            nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
---
+++ train.py
@@ -0,0 +1,74 @@
+import os
+
+import numpy as np
+
+import torch
+import torch.optim as optim
+from torch.nn.parallel import DistributedDataParallel
+from torch.distributed import init_process_group, destroy_process_group
+
+import torchvision.transforms as transforms
+
+from model import EncoderDecoder
+
+device = 'cuda'
+
+# check if ddp run
+ddp = int(os.environ.get('RANK', -1)) != -1
+
+if ddp:
+    # ddp setup
+    init_process_group(backend='nccl')
+    global_rank = int(os.environ['RANK'])               # global rank
+    local_rank = int(os.environ['LOCAL_RANK'])          # local rank
+    device = f'cuda:{local_rank}'
+    master_process = global_rank == 0                   # this process will perform logging etc.
+    seed_offset = global_rank
+else:
+    # single process, single gpu run
+    master_process = True
+    seed_offset = 0
+
+# init data transform
+transform = transforms.Compose([
+    transforms.Resize(254), 
+    transforms.Normalize([0.5], [0.5]),
+])
+
+# loading model to device
+model = EncoderDecoder()
+model.to(device)
+
+# init loss function and optimizer
+criterion = torch.nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+# wrap model in DDP
+if ddp:
+    model = DistributedDataParallel(model, device_ids=[local_rank])
+
+# start training
+epochs = 100
+for e in range(epochs):
+    # random input tensor
+    x = transform(torch.randn(
+        1, 3, 284, 284,
+        requires_grad=True, 
+        device=device,
+    ))
+
+    # forward pass
+    y = model(x)
+
+    # backward pass
+    loss = criterion(y, x)
+    loss.backward()
+    optimizer.step()
+
+    # log loss if master_process
+    if master_process:
+        print(f"Epoch: {e+1} | Loss: {loss:.4f}")
+
+# ddp cleanup
+if ddp:
+    destroy_process_group()
