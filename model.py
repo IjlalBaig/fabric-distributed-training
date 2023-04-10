8f14654e5403277d7f59e296531496cac844b207
zaryabmakram <zaryabmakram@gmail.com>
2023-04-10 16:21:39 +0500

    initial commit

--- model.py
+++ model.py
@@ -1,27 +1,47 @@
 import torch
 import torch.nn as nn
 
-class EncoderDecoder(nn.Module):
-    def __init__(self):
-        super(EncoderDecoder, self).__init__()
-        
-        self.encoder = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-        
-        self.decoder = nn.Sequential(
-            nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1),
-            nn.Tanh()
-        )
+class Generator(nn.Module):
+    def __init__(self, z_dim=10, hidden_dim=128, im_dim=784):
+        super(Generator, self).__init__()
 
+        self.encoder = nn.Sequential(
+            self._generator_block(z_dim, hidden_dim),
+            self._generator_block(hidden_dim, hidden_dim*2),
+            self._generator_block(hidden_dim*2, hidden_dim*4),
+            nn.Linear(hidden_dim*4, im_dim),
+            nn.Sigmoid(),
+        )
+    
+    def _generator_block(self, input_dim, output_dim):
+        return nn.Sequential(
+            nn.Linear(input_dim, output_dim),
+            nn.BatchNorm1d(output_dim),
+            nn.ReLU(inplace=True),
+        )
+    
     def forward(self, x):
         x = self.encoder(x)
+        return x
+
+class Discriminator(nn.Module):
+    def __init__(self, im_dim=784, hidden_dim=128):
+        super(Discriminator, self).__init__()
+
+        self.decoder = nn.Sequential(
+            self._discriminator_block(im_dim, hidden_dim*4),
+            self._discriminator_block(hidden_dim*4, hidden_dim*2),
+            self._discriminator_block(hidden_dim*2, hidden_dim),
+            nn.Linear(hidden_dim, 1),
+        )
+    
+    def _discriminator_block(self, input_dim, output_dim):
+        return nn.Sequential(
+            nn.Linear(input_dim, output_dim),
+            nn.BatchNorm1d(output_dim),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        )
+    
+    def forward(self, x):
         x = self.decoder(x)
         return x
--- train.py
+++ train.py
@@ -9,9 +9,7 @@ import torch.optim as optim
 from torch.nn.parallel import DistributedDataParallel
 from torch.distributed import init_process_group, destroy_process_group
 
-import torchvision.transforms as transforms
-
-from model import EncoderDecoder
+from model import Generator, Discriminator
 
 device = 'cuda'
 
@@ -26,7 +24,7 @@ parser.add_argument(
     type=float, 
     help='learning rate',
     required=False,
-    default=0.001,
+    default=0.00001,
 )
 
 args = parser.parse_args()
@@ -47,48 +45,81 @@ else:
     master_process = True
     seed_offset = 0
 
-# init data transform
-transform = transforms.Compose([
-    transforms.Resize(254), 
-    transforms.Normalize([0.5], [0.5]),
-])
+# init params
+z_dim = 64
+batch_size=6
+epochs = 200
 
 # loading model to device
-model = EncoderDecoder()
-model.to(device)
+gen = Generator(z_dim).to(device)
+disc = Discriminator().to(device)
 
 # init loss function and optimizer
-criterion = torch.nn.MSELoss()
+criterion = nn.BCEWithLogitsLoss()
 
 learning_rate = args.lr
-optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+gen_opt = optim.Adam(gen.parameters(), lr=learning_rate)
+disc_opt = optim.Adam(disc.parameters(), lr=learning_rate)
 
 # wrap model in DDP
 if ddp:
-    model = DistributedDataParallel(model, device_ids=[local_rank])
-    model = nn.SyncBatchNorm.convert_sync_batchnorm(model)
+    gen = DistributedDataParallel(gen, device_ids=[local_rank])
+    gen = nn.SyncBatchNorm.convert_sync_batchnorm(gen)
+
+    disc = DistributedDataParallel(disc, device_ids=[local_rank])
+    disc = nn.SyncBatchNorm.convert_sync_batchnorm(disc)
+
+# training utilities
+def get_noise(n_samples, z_dim, device='cpu'):
+    return torch.randn(n_samples, z_dim, device=device)
+
+def get_disc_loss(gen, disc, criterion, real, num_images, z_dim, device):
+    # fake images
+    noise_vec = get_noise(num_images, z_dim=z_dim, device=device)
+    out_gen = gen(noise_vec).detach()
+    out_dfake = disc(out_gen)
+    loss_dfake = criterion(out_dfake, torch.zeros(num_images, 1, device=device))
+
+    # real images
+    out_dreal = disc(real)
+    loss_dreal = criterion(out_dreal, torch.ones(num_images, 1, device=device))
+
+    # total loss
+    disc_loss = (loss_dfake + loss_dreal) / 2
+
+    return disc_loss
+
+def get_gen_loss(gen, disc, criterion, num_images, z_dim, device):
+    noise_vec = get_noise(num_images, z_dim=z_dim, device=device)
+    out_gen = gen(noise_vec)
+    out_disc = disc(out_gen)
+
+    gen_loss = criterion(out_disc, torch.ones(num_images, 1, device=device))
+    return gen_loss
 
 # start training
-epochs = 1000
 for e in range(epochs):
-    # random input tensor
-    x = transform(torch.randn(
-        1, 3, 284, 284,
-        requires_grad=True, 
-        device=device,
-    ))
+    # placeholder data: using random tensor as real data 
+    real = torch.randn(batch_size, 784).to(device)
 
-    # forward pass
-    y = model(x)
+    # discriminator update
+    disc_opt.zero_grad()
+    disc_loss = get_disc_loss(gen, disc, criterion, real, batch_size, z_dim, device)
+    disc_loss.backward(retain_graph=True)
+    disc_opt.step()
 
-    # backward pass
-    loss = criterion(y, x)
-    loss.backward()
-    optimizer.step()
+    # generator update
+    gen_opt.zero_grad()
+    gen_loss = get_gen_loss(gen, disc, criterion, batch_size, z_dim, device)
+    gen_loss.backward(retain_graph=True)
+    gen_opt.step()
 
-    # log loss if master_process
     if master_process:
-        print(f"Epoch={e+1}; Loss={loss:.4f};")
+        print(
+            f"Epoch: {e+1:0=3d}"
+            f" | Generator Loss: {gen_loss.item():.4f}"
+            f" | Discriminator Loss: {disc_loss.item():.4f}"
+        )
 
 # ddp cleanup
 if ddp:
