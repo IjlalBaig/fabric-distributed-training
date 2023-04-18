0f0aa5498385cf4d32bb3c8c5a89132eb7e84506
ijlalbaig <ijlalbaig92@gmail.com>
2023-04-18 15:01:16 +0500

    Add feature: WGAN

--- model.py
+++ model.py
@@ -1,48 +1,147 @@
 import torch
 import torch.nn as nn
+import lightning as L
 
 
-class Generator(nn.Module):
-    def __init__(self, z_dim=10, hidden_dim=128, im_dim=784):
-        super(Generator, self).__init__()
+class Generator(torch.nn.Module):
+    def __init__(self, channel_multiplier=1):
+        super().__init__()
 
-        self.encoder = nn.Sequential(
-            self._generator_block(z_dim, hidden_dim),
-            self._generator_block(hidden_dim, hidden_dim*2),
-            self._generator_block(hidden_dim*2, hidden_dim*4),
-            nn.Linear(hidden_dim*4, im_dim),
-            nn.Sigmoid(),
+        self.main = nn.Sequential(
+            nn.ConvTranspose2d(100, 512 * channel_multiplier, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(512 * channel_multiplier),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(512 * channel_multiplier, 256 * channel_multiplier, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(256 * channel_multiplier),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(256 * channel_multiplier, 128 * channel_multiplier, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(128 * channel_multiplier),
+            nn.ReLU(True),
+
+            nn.ConvTranspose2d(128 * channel_multiplier, 3, 4, 2, 1),
+            nn.Tanh()
         )
-    
-    def _generator_block(self, input_dim, output_dim):
-        return nn.Sequential(
-            nn.Linear(input_dim, output_dim),
-            nn.BatchNorm1d(output_dim),
-            nn.ReLU(inplace=True),
-        )
-    
-    def forward(self, x):
-        x = self.encoder(x)
-        return x
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        out = self.main(input)
+        return out
+
 
 class Discriminator(nn.Module):
-    def __init__(self, im_dim=784, hidden_dim=128):
+    def __init__(self, channel_multiplier=1):
         super(Discriminator, self).__init__()
 
-        self.decoder = nn.Sequential(
-            self._discriminator_block(im_dim, hidden_dim*4),
-            self._discriminator_block(hidden_dim*4, hidden_dim*2),
-            self._discriminator_block(hidden_dim*2, hidden_dim),
-            nn.Linear(hidden_dim, 1),
+        self.main = nn.Sequential(
+            nn.Conv2d(3, 64 * channel_multiplier, 4, 2, 1, bias=False),
+            nn.LeakyReLU(0.2, True),
+
+            nn.Conv2d(64 * channel_multiplier, 128 * channel_multiplier, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(128 * channel_multiplier, affine=True),
+            nn.LeakyReLU(0.2, True),
+
+            nn.Conv2d(128 * channel_multiplier, 256 * channel_multiplier, 4, 2, 1, bias=False),
+            nn.InstanceNorm2d(256 * channel_multiplier, affine=True),
+            nn.LeakyReLU(0.2, True),
+
+            nn.Conv2d(256 * channel_multiplier, 1, 4, 1, 0),
         )
-    
-    def _discriminator_block(self, input_dim, output_dim):
-        return nn.Sequential(
-            nn.Linear(input_dim, output_dim),
-            nn.BatchNorm1d(output_dim),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True)
-        )
-    
-    def forward(self, x):
-        x = self.decoder(x)
-        return x
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        out = self.main(input)
+        out = torch.flatten(out)
+        return out
+
+
+class WGAN_GP(nn.Module):
+    def __init__(self, fabric: L.Fabric, lr: float = 1e-5, noise_dim: int = 100, channel_multiplier: int = 1):
+        super().__init__()
+        self.fabric = fabric
+        self.lr = lr
+        self.noise_dim = noise_dim
+        self.channel_multiplier = channel_multiplier
+
+        self.gen, self.critic = self.prepare_modules()
+        self.optimizer_gen, self.optimizer_critic = self.prepare_optimizers()
+
+    def prepare_modules(self):
+        gen = Generator(self.channel_multiplier)
+        gen = self.fabric.setup_module(gen)
+
+        critic = Discriminator(self.channel_multiplier)
+        critic = self.fabric.setup_module(critic)
+        return gen, critic
+
+    def prepare_optimizers(self):
+        optimizer_g = torch.optim.Adam(self.gen.parameters(), lr=self.lr)
+        optimizer_g = self.fabric.setup_optimizers(optimizer_g)
+
+        optimizer_c = torch.optim.Adam(self.critic.parameters(), lr=self.lr)
+        optimizer_c = self.fabric.setup_optimizers(optimizer_c)
+        return optimizer_g, optimizer_c
+
+    def gradient_penalty(self, real_images, fake_images):
+        # Random weight term for interpolation between real and fake data
+        alpha = torch.randn((real_images.size(0), 1, 1, 1), device=real_images.device)
+        # Get random interpolation between real and fake data
+        interp_images = (alpha * real_images + ((1 - alpha) * fake_images.data)).requires_grad_(True)
+
+        critique = self.critic(interp_images)
+        grad_outputs = torch.ones(critique.size(), device=real_images.device, requires_grad=False)
+
+        # Get gradient w.r.t. interp_images
+        gradients = torch.autograd.grad(
+            outputs=critique,
+            inputs=interp_images,
+            grad_outputs=grad_outputs,
+            create_graph=True,
+            retain_graph=True,
+            only_inputs=True,
+        )[0]
+        gradients = gradients.view(gradients.size(0), -1)
+        gradient_penalty = torch.mean((gradients.norm(2, dim=1) - 1) ** 2)
+        return gradient_penalty
+
+    def critic_training_step(self, real_images: torch.Tensor, noise: torch.Tensor, fabric: L.Fabric):
+        self.optimizer_critic.zero_grad()
+
+        # Critique real images
+        real_critique = self.critic(real_images)
+        err_critic_real = -torch.mean(real_critique)
+
+        # Critique fake images
+        fake_images = self.gen(noise)
+        fake_critique = self.critic(fake_images.detach())
+        err_critic_fake = torch.mean(fake_critique)
+
+        # Gradient penalty
+        # gp = self.gradient_penalty(real_images, fake_images)
+
+        # Combine losses
+        loss = err_critic_real + err_critic_fake # + gp * 10
+
+        # Backward and optimize
+        fabric.backward(loss)
+        self.optimizer_critic.step()
+
+        return dict(critic_loss=loss)
+
+    def generator_training_step(self, noise: torch.Tensor, fabric: L.Fabric):
+        self.optimizer_gen.zero_grad()
+
+        # Critique fake images
+        fake_images = self.gen(noise)
+        fake_critique = self.critic(fake_images.detach())
+        loss = - torch.mean(fake_critique)
+
+        # Backward and optimize
+        fabric.backward(loss)
+        self.optimizer_gen.step()
+
+        return dict(gen_loss=loss)
+
+    def forward(self, noise: torch.Tensor):
+        return self.gen(noise)
+
+
--- train.py
+++ train.py
@@ -1,127 +1,45 @@
-import os
-import argparse
-
-import numpy as np
-
+from model import WGAN_GP
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.nn.parallel import DistributedDataParallel
-from torch.distributed import init_process_group, destroy_process_group
+from lightning.fabric.strategies import FSDPStrategy, DDPStrategy
+from torch.utils.data import DataLoader
+torch.manual_seed(0)
+import lightning as L
+from torchvision.datasets import CIFAR10
+import torchvision.transforms as T
 
-from model import Generator, Discriminator
+if __name__ == "__main__":
+    BATCH_SIZE = 64
+    NUM_WORKERS = 8
+    NUM_EPOCHS = 1000
+    NUM_CRITIC_STEPS = 1
 
-device = 'cuda'
+    # Prepare Distributed Training Strategy
+    strategy = FSDPStrategy()
+    # strategy = DDPStrategy(find_unused_parameters=True)
+    # strategy = "auto"
+    fabric = L.Fabric(strategy=strategy, devices=[0])
+    fabric.launch()
 
-# init argparse
-parser = argparse.ArgumentParser(
-    description='Pytorch DDP',
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter
-)
+    train_set = CIFAR10(root='./data', train=True, download=True, transform=lambda x: T.ToTensor()(x))
+    train_loader = DataLoader(dataset=train_set, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
+    train_loader = fabric.setup_dataloaders(train_loader)
 
-parser.add_argument(
-    '--lr',
-    type=float, 
-    help='learning rate',
-    required=False,
-    default=0.00001,
-)
+    # model
+    model = WGAN_GP(fabric, lr=1e-5, noise_dim=100, channel_multiplier=12)
 
-args = parser.parse_args()
+    # Training loop
+    for epoch in range(NUM_EPOCHS):
+        fabric.print("Epoch:", epoch)
 
-# check if ddp run
-ddp = int(os.environ.get('RANK', -1)) != -1
+        losses_gen = dict(gen_loss=0.)
+        for i, batch in enumerate(train_loader):
+            real_images, _ = batch
+            n = real_images.size(0)
+            noise = torch.randn(n, 100, 1, 1, device=real_images.device)
 
-if ddp:
-    # ddp setup
-    init_process_group(backend='nccl')
-    global_rank = int(os.environ['RANK'])               # global rank
-    local_rank = int(os.environ['LOCAL_RANK'])          # local rank
-    device = f'cuda:{local_rank}'
-    master_process = global_rank == 0                   # this process will perform logging etc.
-    seed_offset = global_rank
-else:
-    # single process, single gpu run
-    master_process = True
-    seed_offset = 0
+            losses_critic = model.critic_training_step(real_images, noise, fabric)
+            if (i + 1) % NUM_CRITIC_STEPS == 0:
+                losses_gen = model.generator_training_step(noise, fabric)
 
-# init params
-z_dim = 64
-batch_size = 6
-epochs = 200
-
-
-# loading model to device
-gen = Generator(z_dim).to(device)
-disc = Discriminator().to(device)
-
-# init loss function and optimizer
-criterion = nn.BCEWithLogitsLoss()
-
-learning_rate = args.lr
-gen_opt = optim.Adam(gen.parameters(), lr=learning_rate)
-disc_opt = optim.Adam(disc.parameters(), lr=learning_rate)
-
-# wrap model in DDP
-if ddp:
-    gen = DistributedDataParallel(gen, device_ids=[local_rank])
-    gen = nn.SyncBatchNorm.convert_sync_batchnorm(gen)
-
-    disc = DistributedDataParallel(disc, device_ids=[local_rank])
-    disc = nn.SyncBatchNorm.convert_sync_batchnorm(disc)
-
-# training utilities
-def get_noise(n_samples, z_dim, device='cpu'):
-    return torch.randn(n_samples, z_dim, device=device)
-
-def get_disc_loss(gen, disc, criterion, real, num_images, z_dim, device):
-    # fake images
-    noise_vec = get_noise(num_images, z_dim=z_dim, device=device)
-    out_gen = gen(noise_vec).detach()
-    out_dfake = disc(out_gen)
-    loss_dfake = criterion(out_dfake, torch.zeros(num_images, 1, device=device))
-
-    # real images
-    out_dreal = disc(real)
-    loss_dreal = criterion(out_dreal, torch.ones(num_images, 1, device=device))
-
-    # total loss
-    disc_loss = (loss_dfake + loss_dreal) / 2
-
-    return disc_loss
-
-def get_gen_loss(gen, disc, criterion, num_images, z_dim, device):
-    noise_vec = get_noise(num_images, z_dim=z_dim, device=device)
-    out_gen = gen(noise_vec)
-    out_disc = disc(out_gen)
-
-    gen_loss = criterion(out_disc, torch.ones(num_images, 1, device=device))
-    return gen_loss
-
-# start training
-for e in range(epochs):
-    # placeholder data: using random tensor as real data 
-    real = torch.randn(batch_size, 784).to(device)
-
-    # discriminator update
-    disc_opt.zero_grad()
-    disc_loss = get_disc_loss(gen, disc, criterion, real, batch_size, z_dim, device)
-    disc_loss.backward(retain_graph=True)
-    disc_opt.step()
-
-    # generator update
-    gen_opt.zero_grad()
-    gen_loss = get_gen_loss(gen, disc, criterion, batch_size, z_dim, device)
-    gen_loss.backward(retain_graph=True)
-    gen_opt.step()
-
-    if master_process:
-        print(
-            f"Epoch: {e+1:0=3d}"
-            f" | Generator Loss: {gen_loss.item():.4f}"
-            f" | Discriminator Loss: {disc_loss.item():.4f}"
-        )
-
-# ddp cleanup
-if ddp:
-    destroy_process_group()
+            if i % 100 == 0:
+                fabric.print(f"losses: gen ({losses_gen['gen_loss']:.2f}), critic ({losses_critic['critic_loss']:.2f})")
